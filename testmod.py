32a59ca59b3c0ab07f2f6bd7b1de4c78d7a4d137
snehal <snehal.saikar@mmit.edu.in>
2020-03-16 12:18:47 +0530

    profile commit

--- testmod.py
+++
@@ -1,4 +0,0 @@
-apikey='AIzaSyByksrWBWJYbWJenGuIhUZXVceTh5sNjqI'
-location_string = 'Mountain View, CA'
-r = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address="%s"&key=%s' % (location_string, apikey))
-print (r)
