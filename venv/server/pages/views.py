bbed62e4fc489899cfc84d06ef7fec3eb41c184c
snehal <snehal.saikar@mmit.edu.in>
2020-04-22 15:36:58 +0530

    front end and hospital pop up

--- venv/server/pages/views.py
+++ venv/server/pages/views.py
@@ -6,6 +6,8 @@ import json
 import requests
 import geocoder
 
+# url = 'https://maps.googleapis.com/maps/api/geocode/json'
+# api_key = 'AIzaSyByksrWBWJYbWJenGuIhUZXVceTh5sNjqI'
 
 config = {
     'apiKey': "AIzaSyCOjXOsWfXTVrUlvbNorZdTyJO3yOCinlI",
@@ -16,20 +18,20 @@ config = {
     'messagingSenderId': "106176267749",
 }
 
+
 firebase = pyrebase.initialize_app(config)
 authe=firebase.auth()
 db = firebase.database()
-
 # Create your views here.
 def map(request, *args, **kwargs):
     data = db.child("diseases").get()
-    data_h = db.child("h").get()
+    data_h = db.child("hospitals").get()
     diseases = json.dumps(data.val())
     h = json.dumps(data_h.val())
     print(diseases)
     print(">>>>",h)
-    return render(request, "map.html", {"data": diseases,"data_h":h})
 
+    return render(request, "map.html", {"data": diseases,"data_h":h})
 
 
 def login(request, *args, **kwargs):
@@ -48,18 +50,17 @@ def profile(request, *args, **kwargs):
     a = a[0]
     a = a['localId']
 
-
+    def listToString(s):
+        str1 = " "
+        return (str1.join(s))
 
     Hospital =db.child("users1").child(a).child("user").child('h_name').get().val()
     email =  db.child("users1").child(a).child("user").child('h_email').get().val()
     address = db.child("users1").child(a).child("user").child('h_address').get().val()
     phno = db.child("users1").child(a).child("user").child('phone').get().val()
-    # dis = c4
-    # vacc =db.child("users").child(a).child("disease").child(c4).child('vaccine').child('v1').get().val()
-    # return render(request, "profile.html",
-    #                {'Hn': Hospital, 'eml': email, 'add': address, 'phn': phno, 'd': dis, 'vac': vacc})
+    vacc=db.child("hospitals").child(Hospital).child('vaccine').get().val()
     return render(request, "profile.html",
-                  {'Hn': Hospital, 'eml': email, 'add': address, 'phn': phno})
+                  {'Hn': Hospital, 'eml': email, 'add': address, 'phn': phno,'vac':vacc[0],'vac1':vacc[1]})
 
 
 
@@ -68,55 +69,55 @@ def postsign(request):
         eml = request.POST.get('email')
         passw = request.POST.get('password')
 
-
         try:
                 user = authe.sign_in_with_email_and_password(eml,passw)
         except:
-                message = "invalid credentials"
+                message = "The Email or Password you entered is incorrect. "
                 return render(request, "login.html", {"msg":message})
         print(user)
         print(user['idToken'])
         session_id = user['idToken']
         request.session['uid'] = str(session_id)
+
+
         return render(request, "afterlogin.html")
 
 
 # REGISTRATION
 def postsignup(request):
-         hname = request.POST.get('Hospital name')
+         hname = request.POST.get('Hname')
          heml = request.POST.get('Email')
          husername = request.POST.get('Username')
          hpassw = request.POST.get('Password')
-         hcpassw = request.POST.get('Confirm Password')
+         hcpassw = request.POST.get('Confirmpassword')
          hcountry = request.POST.get('Country')
          hstate = request.POST.get('State')
          hadd = request.POST.get('Address')
          hpincode = request.POST.get('Pin code')
          hphno = request.POST.get('Contact no')
 
+
          # params = {'sensor': 'false', 'address': 'Mountain View, CA'}
          # r = requests.get(url, params=params)
          # results = r.json()['results']
          # location = results[0]['geometry']['location']
          # location['lat'], location['lng']
-
+         #
          # g = geocoder.google('Mountain View, CA')
-         # g.latlng
+         # print ("--->>", g.latlng)
 
          try:
                user = authe.create_user_with_email_and_password(heml, hpassw)
                uid = user['localId']
 
-
                address={"address":hadd,"Email":heml,"Phone":hphno,"Country":hcountry,"State":hstate}
-               lat={"lat":'27',"lng":'28'}
-               v={"v1":'aaa'}
+               lat={"lat":26.938090,"lng":75.801870}
                data={"h_name":hname,"phone":hphno,"pincode":hpincode,"username":husername,"password":hpassw ,"h_email": heml,"h_address":hadd}
 
+
                # hospital database
-               db.child("h").child(hname).set(address)
-               db.child("h").child(hname).child("location").set(lat)
-               db.child("h").child(hname).child("vaccine").child("vacc").set(v)
+               db.child("hospitals").child(hname).set(address)
+               db.child("hospitals").child(hname).child("location").set(lat)
 
                  # user database
                db.child("users1").child(uid).child("user").set(data)
@@ -137,8 +138,14 @@ def postaftersign(request):
     hfac=request.POST.get('fac')
     hdis = request.POST.get('Dis')
     hvacc = request.POST.get('Vac')
+    hvacc1 = request.POST.get('Vac1')
     hprev=request.POST.get('preventions')
+    hprev1 = request.POST.get('preventions1')
+    hprev2 = request.POST.get('preventions2')
     hsymp=request.POST.get('symp')
+    hsymp1 = request.POST.get('symp1')
+    hsymp2 = request.POST.get('symp2')
+
 
     idtoken = request.session['uid']
     a = authe.get_account_info(idtoken)
@@ -146,17 +153,31 @@ def postaftersign(request):
     a = a[0]
     a = a['localId']
 
-    p={"p1":hprev}
-    s={"s1":hsymp}
-    lat = {"lat": '27', "lng": '28'}
+
+    p=[hprev,hprev1,hprev2]
+    s=[hsymp,hsymp1,hsymp2]
+    lat = {"lat": 22.973423, "lng": 78.656891}
+    v=[hvacc,hvacc1]
 
 
-    db.child("diseases").child(hdis).set(lat)
-    db.child("diseases").child(hdis).child("precautions").set(p)
+    #display vaccine in hospital
+    hname = db.child("users1").child(a).child("user").child('h_name').get().val()
+    hcon=db.child("hospitals").child(hname).child('Country').get().val()
+    db.child("hospitals").child(hname).child("vaccine").set(v)
+
+    #disease database
+    db.child("diseases").child(hdis).child("places").child(hcon).set(lat)
+    db.child("diseases").child(hdis).child("precaution").set(p)
     db.child("diseases").child(hdis).child("symptoms").set(s)
 
 
     d={"Accredation":harc,"Speciality":hspc,"Facility":hfac}
     db.child("users1").child(a).child("user").child("a_info").set(d)
 
+
     return render(request, "afterlogin.html")
+
+
+
+
+
